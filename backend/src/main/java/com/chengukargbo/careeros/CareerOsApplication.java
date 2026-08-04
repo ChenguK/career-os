640ad4758bee0693c15fb51f6dbeedc312463750
Chengusoyane Kargbo <ChenguKargbo@gmail.com>
2026-08-04 03:44:11 -0400

    Initialize React and Spring Boot applications

---
+++ backend/src/main/java/com/chengukargbo/careeros/CareerOsApplication.java
@@ -0,0 +1,13 @@
+package com.chengukargbo.careeros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerOsApplication.class, args);
+	}
+
+}
